c12b50b939c67261c21a9964a3b3f618edfd372d
Javier Costanzi <javier.costanzi@d3sistemas.com.ar>
2020-12-08 16:24:18 -0600

    Improve some styling. Replace get version button for a reload page button.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 const cssVersionInputElement = document.querySelector('#css-version');
 const jsVersionInputElement = document.querySelector('#js-version');
-const getFileVersionsButtonElement = document.querySelector('#get-file-versions');
+const refreshPageButtonElement = document.querySelector('#refresh-page');
 
 cssVersionInputElement.value = cssVersionInputElement.value.split('-v-')[1].replace('.min.css', '');
 jsVersionInputElement.value = jsVersionInputElement.value.split('-v-')[1].replace('.min.js', '');
+
+refreshPageButtonElement.addEventListener('click', event => {
+  window.location.reload();
+});
