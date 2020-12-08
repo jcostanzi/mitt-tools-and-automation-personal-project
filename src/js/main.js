4c04067ac62c60d73d7f5c7460ef47d0592415a9
Javier Costanzi <javier.costanzi@d3sistemas.com.ar>
2020-12-08 17:00:49 -0600

    Add feature to display CSS and JS files.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,30 @@
 const cssVersionInputElement = document.querySelector('#css-version');
 const jsVersionInputElement = document.querySelector('#js-version');
 const refreshPageButtonElement = document.querySelector('#refresh-page');
+const cssFileElement = document.querySelector('#css-file');
+const jsFileElement = document.querySelector('#js-file');
+const loadFilesButtonElement = document.querySelector('#load-files');
+
+const originalCssVersion = cssVersionInputElement.value;
+const originalJsVersion = jsVersionInputElement.value;
 
 cssVersionInputElement.value = cssVersionInputElement.value.split('-v-')[1].replace('.min.css', '');
 jsVersionInputElement.value = jsVersionInputElement.value.split('-v-')[1].replace('.min.js', '');
 
 refreshPageButtonElement.addEventListener('click', event => {
   window.location.reload();
+});
+
+loadFilesButtonElement.addEventListener('click', event => {
+  let xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open('GET', originalCssVersion, false);
+  xmlhttp.send();
+
+  cssFileElement.textContent = xmlhttp.responseText;
+
+  xmlhttp.open('GET', originalJsVersion, false);
+  xmlhttp.send();
+
+  jsFileElement.textContent = xmlhttp.responseText;
 });
